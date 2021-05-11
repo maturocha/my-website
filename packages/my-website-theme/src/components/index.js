ebc9b85b174eba29221db1247501b07e5bf3e40c
Matías <matu.rocha@gmail.com>
2021-05-10 22:50:51 -0300

    feat: Add Tailwind CSS

--- packages/my-website-theme/src/components/index.js
+++ packages/my-website-theme/src/components/index.js
@@ -1,4 +1,5 @@
 import { connect, Global, Head, styled } from "frontity";
+import tw from "tailwind.macro";
 
 /**
  * Theme is the root React component of our theme. The one we will export
@@ -14,13 +15,17 @@ const Theme = ({ state }) => {
         <meta name="description" content={state.frontity.description} />
         <html lang="es" />
       </Head>
-      <div style={{ minHeight: "calc(100vh - 190px)"}}>
-        {/* Add the header of the site. */}
-       <h1>Hi! I'm matur</h1>
-      </div>
+      <Main>
+        <h1>Hi! I'm matur</h1>
+      </Main>
     </>
   );
 };
 
 export default connect(Theme);
+
+const Main = styled.main`
+  ${tw`flex`}
+  
+`;
 
